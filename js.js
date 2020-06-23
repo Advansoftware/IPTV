9e488aa1b35b1ccfa150064bbec37426c84e21b5
bruno <brunoantunes94@hotmail.com>
2020-06-22 21:08:26 -0300

    correção de link

--- js.js
+++ js.js
@@ -2,16 +2,17 @@ var canal = "";
 $('#converte').click(function(){
     var dados = $("#arquivo").val();
     var linhas = dados.split("#EXTINF:-1");
-    for(var i = 1; i < linhas.length; i++){
-      var link =  "<li " + linhas[i]+"></li>";
-      var nome ="NAME=" +$(link).attr("tvg-name")+";";
-      var Turls = linhas[i].split(",");
-      var rl = Turls[1].split("http:");
-      var url = "URL=http:"+rl[1];
 
-      canal += nome+"\n"+url;
-    }
-    console.log(Turls); 
-    var monta = "[Tocom_URL_LIST]\n\n"+canal;
-    $("#resultado").val(monta);
-});
+    for(var i = 1; i < linhas.length; i++){
+        var link =  "<li " + linhas[i]+"></li>";
+        var nome ="\nNAME="+$(link).attr("tvg-name")+"; ";
+        var Turls = linhas[i].split(",");
+        var rl = Turls[1].split("http:");
+        var url = "URL=http:"+ rl[1].trim();
+        canal += nome.concat(url, ';');
+      }
+
+    var monta = "[Tocom_URL_LIST]\n"+canal;
+    $("#resultado").text(monta);
+    });
+ 
